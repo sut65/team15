aa885abba757554cfdaf86213a29ca36a8576bcd
unknown <sirinthip99978@gmail.com>
2023-01-24 17:09:41 +0700

    controller - close #15

---
+++ backend/controller/cupboard.go
@@ -0,0 +1,54 @@
+
+package controller
+
+import (
+	"github.com/sut65/team15/entity"
+
+	"github.com/gin-gonic/gin"
+
+	"net/http"
+)
+
+// POST /cupboard
+func CreateCupboard(c *gin.Context) {
+	var cupboard entity.Cupboard
+	if err := c.ShouldBindJSON(&cupboard); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := entity.DB().Create(&cupboard).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"data": cupboard})
+}
+
+// GET /cupboard/:id
+func GetCupboard(c *gin.Context) {
+	var cupboard entity.Cupboard
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM cupboards WHERE id = ?", id).Find(&cupboard).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if cupboard.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "id not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": cupboard})
+}
+
+//List
+func ListCupboard(c *gin.Context) {
+
+	var cupboard []entity.Medicine
+	if err := entity.DB().Raw("SELECT * FROM cupboards").Find(&cupboard).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": cupboard})
+
+}
