107265df86c562858cc9159c97f8123a89a68b77
unknown <sirinthip99978@gmail.com>
2023-01-30 13:41:55 +0700

    create entity prescription - close #114

---
+++ backend/entity/prescription.go
@@ -0,0 +1,31 @@
+package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+type Patient struct {
+	gorm.Model
+
+	FirstName	string
+	LastName	string
+	Sex   		string
+	Age   		int
+	Prescription	[]Prescription			`gorm:"foreignKey: PatientID"`
+}
+
+type Prescription struct{
+	gorm.Model
+
+	Datetime		time.Time
+	Note            string
+	Number			int
+
+	DoctorID 		*uint
+	Doctor 	  		User
+
+	PatientID		*uint
+	Patient		  	Patient
+
+}
