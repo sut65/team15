cfa0a8bc58f059f195a55e41ec8f5ae1e7ac43c8
@cottoncandyblue <B6201562@g.sut.ac.th>
2023-01-31 16:08:02 +0700

    setup - close #127

--- backend/entity/medicineLabel.go
+++ backend/entity/medicineLabel.go
@@ -11,7 +11,8 @@ type MedicineLabel struct {
 	Instruction string    `valid:"required~Instruction cannot be blank"`
 	Property    string    `valid:"required~Property cannot be blank"`
 	Consumption string    `valid:"range(0|100)~Consumption must be Positive"`
-	Date        time.Time `valid:"notpast~Date not be past"`
+	Date        time.Time 
+	// Date        time.Time  `valid:"notpast~Date not be past"`
 
 	OrderID *uint
 	Order  Order `gorm:"references:id" valid:"-"`
