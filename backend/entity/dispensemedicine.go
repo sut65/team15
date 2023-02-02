4acdc6b368153be500f3a593b0a8fc82c9b5c67c
unknown <fern2275nfn@gmail.com>
2023-02-02 23:49:32 +0700

    validate receivename - close #134

--- backend/entity/dispensemedicine.go
+++ backend/entity/dispensemedicine.go
@@ -16,7 +16,7 @@ type DispenseMedicine struct {
 	gorm.Model
 
 	DispenseNo			uint					`valid:"matches(^\\d{6}$)~DispenseNo dose not validate as matches(^\\d{6}$), required~DispenseNo: non zero value required, range(100000|999999)~DispenseNo: range 100000|999999"`
-	ReceiveName			string					//`valid:"required~ReceiveName cannot be blank"`
+	ReceiveName			string					`valid:"required~ReceiveName cannot be blank"`
 	DispenseTime		time.Time
 
 	PharmacyID 			*uint
