419cd1cad6751f67905dd7b646499d32375de9ab
unknown <fern2275nfn@gmail.com>
2023-02-02 23:46:46 +0700

    validate dispenseNo - close #133

--- backend/entity/dispensemedicine.go
+++ backend/entity/dispensemedicine.go
@@ -9,14 +9,14 @@ import (
 type Pharmacy struct {
 	gorm.Model
 
-	PharmacyBox				int
+	PharmacyBox				uint
 	DispenseMedicines		[]DispenseMedicine `gorm:"foreignkey:PharmacyID"`
 }
 type DispenseMedicine struct {
 	gorm.Model
 
-	DispenseNo			int
-	ReceiveName			string
+	DispenseNo			uint					`valid:"matches(^\\d{6}$)~DispenseNo dose not validate as matches(^\\d{6}$), required~DispenseNo: non zero value required, range(100000|999999)~DispenseNo: range 100000|999999"`
+	ReceiveName			string					//`valid:"required~ReceiveName cannot be blank"`
 	DispenseTime		time.Time
 
 	PharmacyID 			*uint
