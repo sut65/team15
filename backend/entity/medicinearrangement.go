199bba86a698c2b5390330087592de1227640046
unknown <fern2275nfn@gmail.com>
2023-02-15 23:22:54 +0700

    edit validate - close #212

--- backend/entity/medicinearrangement.go
+++ backend/entity/medicinearrangement.go
@@ -9,9 +9,9 @@ import (
 type MedicineArrangement struct {
 	gorm.Model
 
-	MedicineArrangementNo   		uint				`valid:"matches(^\\d{6}$)~MedicineArrangementNo dose not validate as matches(^\\d{6}$), required~MedicineArrangementNo: non zero value required, range(200000|999999)~MedicineArrangementNo: range 200000|999999"`
+	MedicineArrangementNo   		uint				`valid:"range(200000|999999)~MedicineArrangementNo must be 6 digits, required~MedicineArrangementNo must be 6 digits"`
 	Note                    		string				`valid:"required~Note cannot be blank"`
-	MedicineArrangementTime 		time.Time			`valid:"donotpast~MedicineArrangementTime not be past"`
+	MedicineArrangementTime 		time.Time			`valid:"donotpast~MedicineArrangementTime not be past, donotfuture~MedicineArrangementTime must not be in the future"`
 
 	PharmacistID					*uint
 	Pharmacist						User  
@@ -29,5 +29,10 @@ func init() {
 	govalidator.CustomTypeTagMap.Set("donotpast", func(i interface{}, context interface{}) bool {
 		t := i.(time.Time)
 		return t.After(time.Now().Add(time.Minute * -1)) //เวลา > เวลาปัจจุบัน - 1 นาที
+	})
+	govalidator.CustomTypeTagMap.Set("donotfuture", func(i interface{}, context interface{}) bool {
+		t := i.(time.Time)
+		now := time.Now().Add(time.Minute * 10)
+		return t.Before(now) || t.Equal(now)
 	})
 }
