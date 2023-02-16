8d7ad742222bc412c0be81e258e453ffd19491eb
unknown <fern2275nfn@gmail.com>
2023-02-16 15:07:29 +0700

    edit setup,entity - close #226

--- backend/entity/dispensemedicine.go
+++ backend/entity/dispensemedicine.go
@@ -18,7 +18,7 @@ type DispenseMedicine struct {
 
 	DispenseNo			uint					`valid:"range(100000|999999)~DispensemedicineNo must be 6 digits, required~DispensemedicineNo must be 6 digits"`
 	ReceiveName			string					`valid:"required~ReceiveName cannot be blank"`
-	DispenseTime		time.Time				`valid:"donotpast~DispenseTime not be past"`
+	DispenseTime		time.Time				`valid:"donotpast~DispenseTime not be past, donotfuture~DispenseTime must not be in the future"`
 
 	PharmacyID 			*uint
 	Pharmacy			Pharmacy				`gorm:"references:id" valid:"-"`
