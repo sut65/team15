f41b830b1cd4f654ed55aaf2b910490b5df02544
unknown <B6221928@g.sut.ac.th>
2023-02-22 17:41:28 +0700

    MRD Validation date - close #288

--- backend/entity/medicineDisbursement_validation_test.go
+++ backend/entity/medicineDisbursement_validation_test.go
@@ -45,3 +45,49 @@ func TestNotMedicineDisbursementUNnsable(t *testing.T) {
 		g.Expect(err.Error()).To(Equal("MedicineDisNo must be C then 3 digit"))
 	}
 }
+
+//ในช่วง 1-1000
+func TestMedicineDisAmountnotbeBlank(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	k := []uint{
+		0,
+		1001,
+	}
+
+	for _, ku := range k {
+		medicineDisbursement := MedicineDisbursement{
+
+			MedicineDisNo: "C555",
+			MedicineDisAmount: ku,
+			Dtime:      time.Now(),
+		}
+		// ตรวจสอบด้วย govalidator
+		ok, err := govalidator.ValidateStruct(medicineDisbursement)
+
+	g.Expect(ok).ToNot(BeTrue()) 
+
+	g.Expect(err).ToNot(BeNil()) 
+
+	g.Expect(err.Error()).To(Equal("The MedicineDisAmount must be in the range 1-1000")) 
+
+	}
+
+}
+
+
+func TestMedicineDisbursementUNnsable(t *testing.T) {
+	g := NewGomegaWithT(t)
+	t.Run("check Dtime unsable", func(t *testing.T) {
+		medicineDisbursement := MedicineDisbursement{
+			MedicineDisNo: "C205",
+			MedicineDisAmount: 100,
+			Dtime:      time.Now().Add(time.Minute * -5), // ต้องเป็นปัจจุบัน
+		}
+
+		ok, err := govalidator.ValidateStruct(medicineDisbursement)
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error()).To(Equal("Dtime unsable"))
+	})
+}
