721c866e82a62ef546a493e965b3e1d21c90faf7
unknown <fern2275nfn@gmail.com>
2023-02-24 17:27:20 +0700

    edit list - close #319

--- frontend/src/components/MedicineArrangement.tsx
+++ frontend/src/components/MedicineArrangement.tsx
@@ -23,7 +23,10 @@ function MedicineArrangements() {
         const apiUrl = "http://localhost:8080/medicinearrangements";
         const requestOptions = {
             method: "GET",
-            headers: { "Content-Type": "application/json" },
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json",
+              },
         };
         fetch(apiUrl, requestOptions)
             .then((response) => response.json())
