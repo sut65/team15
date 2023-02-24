aadffa1ab6283dfb988f1f3b50bd4d92fb8a4c5e
Jay <111434827+Woottikrai@users.noreply.github.com>
2023-02-24 17:51:57 +0700

    edit Ui -close #328

--- frontend/src/components/DiscardmedicineUpdate.tsx
+++ frontend/src/components/DiscardmedicineUpdate.tsx
@@ -240,7 +240,7 @@ export default function DiscardmedicineUpdate() {
                     <FormControl fullWidth variant="outlined" style={{ width: '100%', float: 'left' }}>
                         <p>เลขคลังยา</p>
                         <Select
-                            disabled
+                            
                             native
                             value={discard.MedicineReceiveID}
                             onChange={handleChange}
